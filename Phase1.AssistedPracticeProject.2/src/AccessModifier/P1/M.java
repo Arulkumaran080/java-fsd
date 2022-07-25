a11346c1bec7fe5704d51d4b4238007381e796ba
Arulkumaran080 <kumaranarul080@gmail.com>
2022-07-25 23:40:06 +0530

    Practice

---
+++ Phase1.AssistedPracticeProject.2/src/AccessModifier/P1/M.java
@@ -0,0 +1,14 @@
+package AccessModifier.P1;
+
+public class M {
+	private int i=5;
+	long l=23442343;
+	protected float f=3.3f;
+	public void methodm()
+	{
+		System.out.println("we are in class M");
+		System.out.println("value of i "+i);
+		System.out.println("value of l "+l);
+		System.out.println("value of f "+f);
+	}
+}
