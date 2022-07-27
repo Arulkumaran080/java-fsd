8a46628f9499329a5c06b5bb69781e788d7b6c1a
Arulkumaran080 <kumaranarul080@gmail.com>
2022-07-27 11:15:10 +0530

    Practice

--- Phase 1- Pracrice Project 2/src/mail/validation/mail.java
+++ Phase 1- Pracrice Project 2/src/mail/validation/mail.java
@@ -1,38 +1,30 @@
 package mail.validation;
-import java.util.regex.*;
+
 import java.util.*;
 public class mail {
+    
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Email to be find: ");
+		String str = sc.next();
+	  ArrayList<String> mail = new ArrayList<>();
+      
+      mail.add("arulkumaran@gmail.com");
+      mail.add("kumar00@gmail.com");
+      mail.add("keerthi@yahoo.com");
+      mail.add("vikram@gmail.com");
+      mail.add("bala@hotmailcom");
+      mail.add("ramya@gmail.com.com");
 
-	public static boolean isValid(String email)
-    {
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
-                            "[a-zA-Z0-9_+&*-]+)*@" +
-                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                            "A-Z]{2,7}$";
-                              
-        Pattern pat = Pattern.compile(emailRegex);
-        if (email == null)
-            return false;
-        return pat.matcher(email).matches();
-    }
-  
-    public static void main(String[] args)
-    {
-        ArrayList<String> mail = new ArrayList<>();
-            
-          mail.add("arul-^&kumaraan@gmail.com");
-          mail.add("kumar00@gmail.com");
-          mail.add("keerthi@yahoo.com");
-          mail.add("@gmail.com");
-          mail.add("abcd@hotmailcom");
-          mail.add("xyz@abc.co.in");
-            
-        for(String i : mail){
-            if (isValid(i))
-                System.out.println(i + " - Valid");
-            else
-                System.out.println(i + " - Invalid");
-        }
-    }
-
+	  //Load the list into a hashSet
+	  HashSet<String> set = new HashSet<String>(mail);
+	  if (set.contains(str))
+	  {
+	    System.out.println("Email is found ");
+	  }
+	  else
+	  {
+		  System.out.println("Email is not found, try with another id");
+	  }
+	}
 }
